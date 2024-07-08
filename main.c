bc02f8e6733beb9ff9589505fe35153b460209aa
nightwalker59ru <kostya.myakotnikov1922@gmail.com>
2024-07-08 23:43:29 +0500

    Give a personalized hello.

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+#include <stdio.h>
+
+
 
 int main(int argc, char **argv)
 
 {
 
-printf("Hello world!\n");
+char name[255];
+
+printf("Enter your name: ");
+
+fgets(name, 255, stdin);
+
+
+printf("Hello %s!\n", name);
 
 return 0;
-
 }
-
